f7b8c0dc344732d6a3f236636c7e4abb812ada0a
Harakiric <Simon.jonsson.91@live.se>
2022-04-13 21:55:43 +0200

    sv locale

--- locale/sv.lua
+++ locale/sv.lua
@@ -7,7 +7,10 @@ local Translations = {
         no_access = 'Du har inte tillgång till detta kommando',
         company_too_poor = 'Din arbetsgivare är pank',
         item_not_exist = 'Objektet finns inte',
-        too_heavy = 'Din inventory är full!'
+        too_heavy = 'Din inventory är full!',
+        duplicate_license = 'Dubbel "Rockstar License" Hittad',
+        no_valid_license  = 'Ingen Rockstar License Hittad',
+        not_whitelisted = 'Du är inte allowlistad på den här servern'
     },
     success = {},
     info = {
@@ -15,7 +18,10 @@ local Translations = {
         job_info = 'Jobb: %{value} | Grad: %{value2} | Tjänst: %{value3}',
         gang_info = 'Gäng: %{value} | Grad: %{value2}',
         on_duty = 'Du är nu i tjänst!',
-        off_duty = 'Du har gått ur tjänst!'
+        off_duty = 'Du har gått ur tjänst!',
+        checking_ban = 'Hej %s. Vi kollar om du är bannad.',
+        join_server = 'Välkommen %s till {Server Name}.',
+        checking_whitelisted = 'Hej %s. Vi kollar om du är allowlistad.'
     }
 }
 
